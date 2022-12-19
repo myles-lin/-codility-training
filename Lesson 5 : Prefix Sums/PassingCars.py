c4a013a867d953f2a9534a85ee0267a9ca298e66
myles-lin <91050917+myles-lin@users.noreply.github.com>
2022-12-19 19:29:50 +0800

    Update PassingCars.py

--- Lesson 5 : Prefix Sums/PassingCars.py
+++ Lesson 5 : Prefix Sums/PassingCars.py
@@ -3,7 +3,7 @@ def solution(cars):
     passing_cnt = 0
     for i in range(len(cars)):
         if cars[i] == 0:
-          # passing_count += cars[i+1:].count(1)  # 若使用此方法, cars = [0, 1] * 100000, higher big O time complexity.
+          # passing_cnt += cars[i+1:].count(1)  # 若使用此方法, cars = [0, 1] * 100000, higher big O time complexity.
             passing_cnt += remaining_one_cnt
             if passing_cnt > 1000000000:
                 return -1
